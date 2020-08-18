972da80ede46545265d62f8ccb2beb1a4854af03
triton <tmit2010@gmail.com>
2020-08-18 21:32:05 +0900

    update

--- abc/173/b.py
+++ abc/173/b.py
@@ -1,11 +1,4 @@
-import collections
-
 N = int(input())
-judgement = []
-for i in range(N):
-    judgement.append(input())
-c = collections.Counter(judgement)
-
-order = ["AC", "WA", "TLE", "RE"]
-for ans in order:
-    print(ans + " x " + str(c[ans]))
+s = [input() for i in range(N)]
+for v in ["AC", "WA", "TLE", "RE"]:
+    print('{0} x {1}'.format(v, s.count(v)))
--- abc/173/c.py
+++ abc/173/c.py
@@ -1,29 +1,24 @@
 import itertools
 H,W,K = map(int, input().split())
-
+ans = 0
 matrix =[]
-rowblack = []
 
 for i in range(H):
     row = list(input())
     matrix.append(row)
-    rowblack.append(row.count('#'))
 
-allblacks = sum(rowblack)
-ans = 0
-
+# パターン列挙
 selectRows = list(itertools.product([0,1], repeat=H))
 selectColumns = list(itertools.product([0,1], repeat=W))
 
 for selectRow in selectRows:
     for selectColumn in selectColumns:
         counter = 0
-
         for row in range(H):
             for column in range(W):
+                # 赤く塗りつぶされない＆黒であった箇所を集計
                 if selectRow[row] == 0 and selectColumn[column] == 0 and matrix[row][column] == '#':
                     counter += 1
-        
         if counter == K:
             ans += 1
 
